40ccbde44b3ca765ac78752d501dba226262b26e
Avil Avate <avilavate123@gmail.com>
2019-08-31 21:30:35 +0530

    pug view engine integration

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ var app = express();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.set("views","./src/views");
+app.set("view engine","pug");
 
+app.get("/", function(req, res){
+   // res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render("index",{list:['avil','suchi'], title:'My App'});
 });
 
 // eslint-disable-next-line no-magic-numbers
 app.listen(3000, () => {
-    debug(`listening on', ${chalk.green('3000')}`);
+    // eslint-disable-next-line no-undef
+    console.log(`listening on port', ${chalk.green('3000')}`);
 });
