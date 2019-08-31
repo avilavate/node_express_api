009458678257de0e33efe3cd95f8c78f773fba5f
Avil Avate <avilavate123@gmail.com>
2019-08-31 14:56:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var chalk = require("chalk");
+var debug = require("debug");
+var morgan = require("morgan");
+var path = require("path");
+
+app = express();
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+
+});
+
+app.listen(3000, () => {
+    console.log(`listening on", ${chalk.green("3000")}`);
+});
