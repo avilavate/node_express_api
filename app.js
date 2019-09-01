6722b07f3edbc03e1a907c74e0154a0f5df3e963
Avil Avate <avilavate123@gmail.com>
2019-09-01 12:44:17 +0530

    added bootstrapozero template - storytrap

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ var path = require("path");
 var app = express();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("css",express.static(path.join(__dirname,"public","css")));
+app.use("js",express.static(path.join(__dirname,"public","js")));
 
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
 app.get("/", function(req, res){
    // res.sendFile(path.join(__dirname, 'views', 'index.html'));
-    res.render("index",{list:['avil','suchi'], title:'My App'});
+    res.render("index",{list:['avil','suchi'], title:'Library'});
 });
 
 // eslint-disable-next-line no-magic-numbers
