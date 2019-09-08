a3e88d9c05df155431939682d688e0296ab4e6fc
Avil Avate <avilavate123@gmail.com>
2019-09-08 20:41:10 +0530

    eslint fixes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 
-const bookRouter=require('./src/bookroutes');
+const bookRouter = require('./src/bookroutes');
 
 
 app.use(morgan('tiny'));
--- src/bookroutes.js
+++ src/bookroutes.js
@@ -4,45 +4,50 @@ const express = require('express');
 const bookRouter = express.Router();
 
 const books = [
-    {
-      title: 'War and pease',
-      genre: 'Hostorical Fiction',
-      Author: 'Lov Tolstoy',
-      read: 'false',
-    },
-    {
-      title: 'War and pease',
-      genre: 'Hostorical Fiction',
-      Author: 'Lov Tolstoy',
-      read: 'false',
-    },
-    {
-      title: 'War and pease',
-      genre: 'Hostorical Fiction',
-      Author: 'Lov Tolstoy',
-      read: 'false',
-    },
-    {
-      title: 'War and pease',
-      genre: 'Hostorical Fiction',
-      Author: 'Lov Tolstoy',
-      read: 'false',
-    },
-  ];
-  
+  {
+    title: 'War and pease',
+    genre: 'Hostorical Fiction',
+    Author: 'Lov Tolstoy',
+    read: 'false',
+  },
+  {
+    title: 'War and pease',
+    genre: 'Hostorical Fiction',
+    Author: 'Lov Tolstoy',
+    read: 'false',
+  },
+  {
+    title: 'War and pease',
+    genre: 'Hostorical Fiction',
+    Author: 'Lov Tolstoy',
+    read: 'false',
+  },
+  {
+    title: 'War and pease',
+    genre: 'Hostorical Fiction',
+    Author: 'Lov Tolstoy',
+    read: 'false',
+  },
+];
+
 
 bookRouter.route('/').get((req, res) => {
-    res.render('books', {
-      nav: [{ title: 'Books', link: '/books' }, { title: 'Authors', link: '/author' }],
-      title: 'Library',
-      books,
-    });
+  res.render('books', {
+    nav: [{ title: 'Books', link: '/books' }, { title: 'Authors', link: '/author' }],
+    title: 'Library',
+    books,
   });
-  
-  bookRouter.route('/single').get((req,res)=>{
-    console.log("Called");
-    res.send("Single book view");
-  });
+});
 
-  module.exports=bookRouter;
-  
+bookRouter.route('/single/:id').get((req, res) => {
+  const { id } = req.params;
+  console.log(`Single book view book id: ${id}`);
+
+  res.render('books', {
+    nav: [{ title: 'Books', link: '/books' }, { title: 'Authors', link: '/author' }],
+    title: 'Library',
+    books: books[id],
+  });
+});
+
+module.exports = bookRouter;
